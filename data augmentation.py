b67fd434ad2978a617476a9e51149f8654c7873b
Saksham975 <33253225+Saksham975@users.noreply.github.com>
2021-08-23 23:42:35 +0530

    Add files via upload

---
+++ data augmentation.py
@@ -0,0 +1,97 @@
+import pathlib
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, cache_dir='.', untar=True)
+
+data_dir = pathlib.Path(data_dir)
+
+flowers_images_dict = {
+    'roses': list(data_dir.glob('roses/*')),
+    'daisy': list(data_dir.glob('daisy/*')),
+    'dandelion': list(data_dir.glob('dandelion/*')),
+    'sunflowers': list(data_dir.glob('sunflowers/*')),
+    'tulips': list(data_dir.glob('tulips/*')),
+}
+
+flowers_labels_dict = {
+    'roses': 0,
+    'daisy': 1,
+    'dandelion': 2,
+    'sunflowers': 3,
+    'tulips': 4,
+}
+
+X, y = [], []
+
+for flower_name, images in flowers_images_dict.items():
+    for image in images:
+        img = cv2.imread(str(image))
+        resized_img = cv2.resize(img, (180, 180))
+        X.append(resized_img)
+        y.append(flowers_labels_dict[flower_name])
+
+X = np.array(X)
+X = X / 255
+y = np.array(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+num_classes = 5
+
+model = Sequential([
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=15)
+
+print(model.evaluate(X_test, y_test))  # very less accuracy on test data - 64%
+
+# Improve Test Accuracy Using Data Augmentation
+data_augmentation = keras.Sequential(
+    [
+        layers.experimental.preprocessing.RandomFlip("horizontal",
+                                                     input_shape=(180, 180, 3)),
+        layers.experimental.preprocessing.RandomRotation(0.1),
+        layers.experimental.preprocessing.RandomZoom(0.1),
+    ]
+)
+
+model = Sequential([
+    data_augmentation,
+    layers.Conv2D(16, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(),
+    layers.Dropout(0.2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs=10)
+print(model.evaluate(X_test, y_test))  # accuracy increased to 72% after data augmentation
